2822383953879cf906da061cb6f8785f38ea050e
Jay Hayes <ur@iamvery.com>
2013-10-14 15:17:28 -0500

    Fix bug in command regex matcher
    * Since "roll me" was optional in the regex, this command was being
      triggered for All The Things™. This is a result of coding late into
      the night. Take note, children. :poop:

--- lib/lita/handlers/die.rb
+++ lib/lita/handlers/die.rb
@@ -4,11 +4,11 @@ module Lita
   module Handlers
     class Die < Handler
 
-      # http://www.rubular.com/r/8UdvqUvTtE
-      route(/(\broll me )?([^,]+)/i, :roll, command: true, help: { 'roll me' => 'roll the die, fate takes over' })
+      # http://www.rubular.com/r/7w0BXzS80a
+      route(/\broll me (.+)/i, :roll, command: true, help: { 'roll me' => 'roll the die, fate takes over' })
 
       def roll(response)
-        choices = response.matches.collect{ |arr| arr[1] }
+        choices = response.matches.flatten.first.split(',')
         response.reply('Performing complex, mathematical randomization...')
         response.reply(%Q(The result: "#{choices.sample.strip}"))
       end
